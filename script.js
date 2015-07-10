548024df3897804cf79d8c762eb7181d9c9a4467
osamaasaid <osamaasaid@gmail.com>
2015-07-09 18:52:45 -0700

    first push

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+ console.log( "ready!" );
+
+    $('.correct').click(function(){
+    	console.log('yes');
+    	$(this).css({'color':'green'})
+
+
+    });
+	
+	// $('.correct').animate('.correct')
+
+ // console.log('good job!!!!!');
+
+
+
+
+});
